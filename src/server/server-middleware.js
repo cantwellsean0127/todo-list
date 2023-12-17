eacffb4726b9fc7c6c3d276dfec0d4593471a38d
Sean Cantwell <cantwellsean0127@gmail.com>
2023-12-16 19:32:08 -0600

    Add a rate limit of 500 requests per minute

--- src/server/server-middleware.js
+++ src/server/server-middleware.js
@@ -4,6 +4,9 @@ import { dirname, join } from "path"
 // The express module contains many middlewares we can add to our server
 import express from "express"
 
+// The express-rate-limit module allows us to add rate limiting to requests
+import express_rate_limit from "express-rate-limit"
+
 // Gets this filepath and directory
 const thisFilePath = import.meta.url
 const thisDirectory = dirname(thisFilePath)
@@ -21,5 +24,11 @@ export default (server) => {
     // Statically serves media files
     static_directory = join(thisDirectory, "../react/media").slice(5)
     server.use(express.static(static_directory))
+
+    // Applies a rate limiting of 100 requests per minute window
+    server.use(express_rate_limit({
+        windowMs: 1 * 60 * 1000,
+        max: 500,
+    }))
 
 }
